fb649a9f1f63ee21dd2b50b9fd64c097f9fd54c9
Millennium-Cleaning <den-4ik_94@mail.ru>
2025-05-12 18:53:08 +0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("order-btn").addEventListener("click", function() {
-  document.getElementById("modal").classList.remove("hidden");
-});
-document.querySelector(".close-button").addEventListener("click", function() {
-  document.getElementById("modal").classList.add("hidden");
-});
